c6f50f52abbae5aad9f2707f9a6eadc13e3d38a2
prodev2023 <rinil.kunhiraman@protonmail.com>
2023-07-19 17:08:43 -0700

    Basic Express Server
     - fetchAPI Endpoint
     - submitAPI Endpoint

---
+++ app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+import crypto from 'crypto'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:5173' }));
+
+function generateUniqueId(length = 16) {
+	return new Promise((resolve, reject) => {
+		crypto.randomBytes(length, (err, buffer) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(buffer.toString('hex'));
+			}
+		});
+	});
+}
+
+let formData = [];
+
+app.post('/api/submitAPI', async (req, res) => {
+	const { date, time, noOfGuests, occasion } = req.body;
+  const uniqueId = await generateUniqueId();
+
+	formData.push({id:uniqueId, date, time, noOfGuests, occasion });
+
+	res.status(200).json(formData);
+});
+
+app.get('/api/fetchAPI', (req, res) => {
+	res.json(formData);
+});
+
+const port = 5500;
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
